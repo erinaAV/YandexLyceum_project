7aa005ce3e7ac3a87efa8b60ad531f71a44448a9
Alina_EV <erina.av@yandex.ru>
2018-12-15 15:13:28 +0300

    Добавлены новые функции

--- calculator.py
+++ calculator.py
@@ -8,22 +8,76 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('design.ui',self)
-        [i.clicked.connect(self.run) for i in self.buttonGroup_digits.buttons()]        
-        self.pushButton_0.clicked.connect(self.input_num, 0)
-        self.pushButton_1.clicked.connect(self.input_num, 1)
-        self.pushButton_2.clicked.connect(self.input_num, 2)
-        self.pushButton_3.clicked.connect(self.input_num, 3)
-        self.pushButton_4.clicked.connect(self.input_num, 4)
-        self.pushButton_5.clicked.connect(self.input_num, 5)
-        self.pushButton_6.clicked.connect(self.input_num, 6)
-        self.pushButton_7.clicked.connect(self.input_num, 7)
-        self.pushButton_8.clicked.connect(self.input_num, 8)
-        self.pushButton_9.clicked.connect(self.input_num, 9)
- 
-    def input_num(self, n):
-        self.lcdNumber.display(n)
- 
- 
+        
+        [i.clicked.connect(self.run) for i in self.buttonGroup_numbers.buttons()]
+        [i.clicked.connect(self.calc) for i in self.buttonGroup_binary.buttons()]
+        self.pushButton_comma.clicked.connect(self.run)
+        self.pushButton_equally.clicked.connect(self.result)
+        self.pushButton_AC.clicked.connect(self.clear)
+        self.pushButton_square.clicked.connect(self.square)
+        self.pushButton_cube.clicked.connect(self.cube)
+        #self.pushButton_factorial.clicked.connect(self.fact)
+        self.data = ''
+        self.data_eval = ''
+        
+        
+    ##реализация кнопки AC на калькуляторе(стирает все введенные ранее данные)
+    def clear(self):
+        self.data = ''
+        self.data_eval = ''
+        self.table.display('') 
+    
+    def run(self):
+        if self.sender().text()==',':
+            if ',' in self.data:
+                return
+        if self.data!='0' or (self.data=='0' and self.sender().text()==','):
+            self.data = self.data + self.sender().text()
+            self.data_eval = self.data_eval + self.sender().text()
+            self.table.display(self.data)
+        else:
+            self.data = self.sender().text()
+            self.data_eval = self.sender().text()
+            self.table.display(self.data)
+            
+    def calc(self):
+        if self.data_eval:
+            self.result()
+            if (self.data_eval[-1] not in ['+','-',':','x', 'x^y']):
+                self.data_eval += self.sender().text()
+            else:
+                self.data_eval = self.data_eval[0:len(self.data_eval)-1] + self.sender().text()
+            self.data_eval = self.data_eval.replace('x^y',
+                                                    '**').replace(':','/').replace('x', '*')
+            
+    ##возведение числа в квадрат        
+    def square(self):
+        if self.data_eval:
+            self.data_eval +='**2'
+            self.result() 
+    
+    ##возведение числа в куб
+    def cube(self):
+        if self.data_eval:
+            self.data_eval +='**(1/3)'
+            self.result()        
+            
+    def result(self):
+        try:
+            float(self.data_eval)
+        except:
+            try:
+
+                self.data = eval(self.data_eval)
+                self.data_eval = str(self.data)
+                self.table.display(self.data)
+            except ZeroDivisionError:
+                self.table.display('Error')
+            except:
+                pass
+        self.data = ''
+    
+        
 app = QApplication(sys.argv)
 ex = MainWindow()
 ex.show()
